b3b98c5600eaca4d07023ff662c57daeea53f200
Ford Arnett <ford.arnett@bottlerocketstudios.com>
2018-08-20 15:21:18 -0500

    Remove unneeded init and variable

--- src/test/java/operations/UserOperationsWeb.java
+++ src/test/java/operations/UserOperationsWeb.java
@@ -7,11 +7,5 @@ import com.bottlerocket.webdriverwrapper.WebDriverWrapper;
  * Created by ford.arnett on 9/3/15.
  */
 public class UserOperationsWeb extends UserOperations {
-    WebDriverWrapper driverWrapper;
-
-    @Override
-    public void init(WebDriverWrapper driverWrapper) {
-        this.driverWrapper = driverWrapper;
-    }
     
 }
